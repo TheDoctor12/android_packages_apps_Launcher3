f4fbe45ae41ca22fc4d5ef0a5d9d5edc236c8113
TheDoctor12 <jkaskabas95@gmail.com>
2022-05-31 22:06:52 +0300

    Revert "Launcher3: Allow enabling recents overview grid on phones"
    This reverts commit cbda3e0815cca77e47c2e7bc57c78c0f55f3bc1c.

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3283,9 +3283,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             return;
         }
         mActionsView.setSplitButtonVisible(
-                mActivity.getDeviceProfile().overviewShowAsGrid &&
-                        mActivity.getDeviceProfile().isTablet &&
-                        getTaskViewCount() > 1);
+                mActivity.getDeviceProfile().overviewShowAsGrid && getTaskViewCount() > 1);
     }
 
     /**
