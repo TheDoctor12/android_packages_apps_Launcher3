868357360a4ac88b33497c3dd8f802f13dc638bf
minaripenguin37 <alexfinhart@gmail.com>
2022-11-13 05:33:52 +0800

    Launcher3: Prevent an UnsupportedOperationException on SpringAnimation
    https://bin.kv2.dev/~636fdd2ae599576ea02b126c
    
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- src/com/android/launcher3/dragndrop/DragView.java
+++ src/com/android/launcher3/dragndrop/DragView.java
@@ -611,6 +611,9 @@ public abstract class DragView<T extends Context & ActivityContext> extends Fram
         }
 
         public void animateToPos(float value) {
+            if (value < -mDelta) {
+              value = -mDelta;
+            }
             mSpring.animateToFinalPosition(Utilities.boundToRange(value, -mDelta, mDelta));
         }
     }
